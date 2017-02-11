7deee59c2e99a139e03b2770e82e156d1f018788
azq2 <azq2@ya.ru>
2017-02-11 04:07:38 +0300

    Исходник актуального бута Chaos с поддержкой флеш, которые в EL71 + скрипты сборки

---
+++ boot/bin2hex.pl
@@ -0,0 +1,17 @@
+#!/bin/perl
+use warnings;
+use strict;
+
+open F, "<".$ARGV[0];
+binmode F;
+while (!eof(F)) {
+	read F, my $buf, 1024;
+	print bin2hex($buf);
+}
+close F;
+
+sub bin2hex {
+	my $hex = shift;
+	$hex =~ s/([\W\w])/sprintf("%02X", ord($1))/ge;
+	return $hex;
+}
