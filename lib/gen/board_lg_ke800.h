ef202e1a1caf082e7a6d717e03662b5d6cb49dfe
Alula <git@alula.me>
2026-07-16 18:45:17 +0200

    LG KE970 & KE800 partial support

---
+++ lib/gen/board_lg_ke800.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <stdint.h>
+
+#define PMB8876
+
+// GPIO numbers
+#define	GPIO_OPEN_CLOSE_SW1	GPIO_DIF_HD
+#define	GPIO_OPEN_CLOSE_SW2	GPIO_MMCI_DAT1
+
+
+// Keypad
+#define	KP_NUM1		0x00000102
+#define	KP_NUM4		0x00000104
+#define	KP_NUM7		0x00000108
+#define	KP_STAR		0x00000110
+#define	KP_NUM2		0x00000202
+#define	KP_NUM5		0x00000204
+#define	KP_NUM8		0x00000208
+#define	KP_NUM0		0x00000210
+#define	KP_END_CALL	0x00000220
+#define	KP_NUM3		0x00000402
+#define	KP_NUM6		0x00000404
+#define	KP_NUM9		0x00000408
+#define	KP_HASH		0x00000410
+#define	KP_SEND		0x00000801
+#define	KP_CLEAR	0x00000802
+
+
---
+++ lib/gen/board_lg_ke970.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <stdint.h>
+
+#define PMB8876
+
+// GPIO numbers
+#define	GPIO_OPEN_CLOSE_SW1	GPIO_DIF_HD
+#define	GPIO_OPEN_CLOSE_SW2	GPIO_MMCI_DAT1
+
+
+// Keypad
+#define	KP_SOFT_LEFT	0x00000101
+#define	KP_NUM1			0x00000102
+#define	KP_NUM4			0x00000104
+#define	KP_NUM7			0x00000108
+#define	KP_STAR			0x00000110
+#define	KP_NAV_LEFT		0x00000120
+#define	KP_NAV_CENTER	0x00000201
+#define	KP_NUM2			0x00000202
+#define	KP_NUM5			0x00000204
+#define	KP_NUM8			0x00000208
+#define	KP_NUM0			0x00000210
+#define	KP_END_CALL		0x00000220
+#define	KP_SOFT_RIGHT	0x00000401
+#define	KP_NUM3			0x00000402
+#define	KP_NUM6			0x00000404
+#define	KP_NUM9			0x00000408
+#define	KP_HASH			0x00000410
+#define	KP_NAV_RIGHT	0x00000420
+#define	KP_SEND			0x00000801
+#define	KP_CLEAR		0x00000802
+#define	KP_VOLUME_UP	0x00000804
+#define	KP_VOLUME_DOWN	0x00000808
+#define	KP_MUSIC		0x00000810
+#define	KP_PTT			0x00000820
+#define	KP_CAMERA		0x00001020
+
+
