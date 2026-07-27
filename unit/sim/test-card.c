e4c2841c3383afa53ff8f13a683fb5a015ff018c
Kirill Zhumarin <kirill.zhumarin@gmail.com>
2026-07-27 18:56:25 +0300

    unit: simplify SIM PMIC setup

--- unit/sim/test-card.c
+++ unit/sim/test-card.c
@@ -31,10 +31,6 @@ struct apdu_response {
 	uint8_t sw2;
 };
 
-static uint8_t saved_pmic_reg06;
-static uint8_t saved_pmic_reg0a;
-static bool pmic_state_saved;
-
 #if defined(SIM_HARDWARE_T0) || defined(SIM_TIMER_TEST)
 
 static const uint8_t T0_HEADER[] __attribute__((aligned(4))) = {
@@ -62,33 +58,6 @@ static void delay_ms(uint32_t milliseconds) {
 
 	while (stopwatch_elapsed_ms(start) < milliseconds)
 		test_watchdog_serve();
-}
-
-static bool pmic_read_byte(uint8_t reg, uint8_t *value) {
-	i2c_start();
-	if (i2c_write(PASIC_I2C_ADDR << 1) || i2c_write(reg))
-		goto no_ack;
-	i2c_stop();
-
-	i2c_start();
-	if (i2c_write((PASIC_I2C_ADDR << 1) | 1))
-		goto no_ack;
-	*value = i2c_read(true);
-	i2c_stop();
-
-	return true;
-
-no_ack:
-	i2c_stop();
-	return false;
-}
-
-static bool pmic_write_byte(uint8_t reg, uint8_t value) {
-	i2c_start();
-	bool acknowledged = !i2c_write(PASIC_I2C_ADDR << 1) && !i2c_write(reg) && !i2c_write(value);
-	i2c_stop();
-
-	return acknowledged;
 }
 
 static void sim_clear_ok(void) {
@@ -141,11 +110,9 @@ static bool sim_receive_procedure(uint8_t *value) {
 }
 
 static bool sim_power_on(uint8_t atr[SIM_MAX_ATR_SIZE], size_t *atr_size) {
+	uint8_t supply_enable;
+
 	i2c_init();
-	if (!pmic_read_byte(PASIC_SUPPLY_ENABLE_1, &saved_pmic_reg06) ||
-		!pmic_read_byte(PASIC_SUPPLY_MODE, &saved_pmic_reg0a))
-		return false;
-	pmic_state_saved = true;
 
 	SIM_CLC = 4 << MOD_CLC_RMC_SHIFT;
 	SIM_CON = 0;
@@ -155,9 +122,9 @@ static bool sim_power_on(uint8_t atr[SIM_MAX_ATR_SIZE], size_t *atr_size) {
 	SIM_IMSC = 0;
 	SIM_ICR = SIM_IRQ_MASK;
 	SIM_CON = SIM_CON_SIMEN;
-	if (!pmic_write_byte(PASIC_SUPPLY_MODE, saved_pmic_reg0a) ||
-		!pmic_write_byte(PASIC_SUPPLY_ENABLE_1, saved_pmic_reg06 | PASIC_SUPPLY_ENABLE_1_VAUDREGA_EN))
-		return false;
+	supply_enable = i2c_smbus_read_byte(PASIC_I2C_ADDR, PASIC_SUPPLY_ENABLE_1);
+	supply_enable |= PASIC_SUPPLY_ENABLE_1_VAUDREGA_EN;
+	i2c_smbus_write_byte(PASIC_I2C_ADDR, PASIC_SUPPLY_ENABLE_1, supply_enable);
 	GPIO_PIN(GPIO_I2C_SCL) = 0x1211;
 	GPIO_PIN(GPIO_I2C_SDA) = 0x1211;
 
@@ -179,14 +146,17 @@ static bool sim_power_on(uint8_t atr[SIM_MAX_ATR_SIZE], size_t *atr_size) {
 }
 
 static void sim_power_off(void) {
+	uint8_t supply_enable;
+
 	SIM_DMAE = 0;
 	SIM_CON = 0;
 	SIM_IRQEN = 0;
 	SIM_ICR = SIM_IRQ_MASK;
-	if (pmic_state_saved) {
-		i2c_init();
-		pmic_write_byte(PASIC_SUPPLY_ENABLE_1, saved_pmic_reg06);
-	}
+
+	i2c_init();
+	supply_enable = i2c_smbus_read_byte(PASIC_I2C_ADDR, PASIC_SUPPLY_ENABLE_1);
+	supply_enable &= ~PASIC_SUPPLY_ENABLE_1_VAUDREGA_EN;
+	i2c_smbus_write_byte(PASIC_I2C_ADDR, PASIC_SUPPLY_ENABLE_1, supply_enable);
 }
 
 #ifdef SIM_HARDWARE_T0
