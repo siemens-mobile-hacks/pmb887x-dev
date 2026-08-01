ddf27e5c9f475081ae700b58caf98546b539feee
Kirill Zhumarin <kirill.zhumarin@gmail.com>
2026-08-01 14:02:22 +0300

    unit: isolate board build directories

--- unit/run.sh
+++ unit/run.sh
@@ -4,14 +4,13 @@ set -x
 
 test_name="$1"
 shift
+board=${BOARD:-siemens-el71}
+build_dir="build/$board"
 
-cmake_args=(-B build -DBOOT=extram -DTEST_COLOR="${TEST_COLOR:-ON}")
-if [[ -n "${BOARD:-}" ]]; then
-	cmake_args+=(-DBOARD="$BOARD")
-fi
+cmake_args=(-B "$build_dir" -DBOARD="$board" -DBOOT=extram -DTEST_COLOR="${TEST_COLOR:-ON}")
 
 cmake "${cmake_args[@]}"
-cmake --build build --target "$test_name"
+cmake --build "$build_dir" --target "$test_name"
 
-chaos_args=(--exec="build/$test_name.bin" --speed=115200)
+chaos_args=(--exec="$build_dir/$test_name.bin" --speed=115200)
 perl ../chaos-boot.pl "${chaos_args[@]}" "$@"
