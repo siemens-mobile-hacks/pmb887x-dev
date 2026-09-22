1499bf56b9bea70acc9ff7bb3ed0c4f847017e9a
Kirill Zhumarin <kirill.zhumarin@gmail.com>
2026-09-22 23:21:51 +0000

    board: fix generic 8875/8876

--- lib/gen/board_generic_pmb8875.h
+++ lib/gen/board_generic_pmb8875.h
@@ -4,7 +4,12 @@
 #define PMB8875
 #define BOARD_NAME "generic-pmb8875"
 
+// Software platform
+#define BOARD_PLATFORM_APOXI 1
+
 // Peripherals
+#define	BOARD_HAS_FLASH0_CFI_FLASH	1
+#define	BOARD_HAS_SDRAM_SDRAM		1
 #define	BOARD_HAS_SIM_CARD_SIM_CARD	1
 
 // GPIO numbers
